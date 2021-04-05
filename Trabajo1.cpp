b53d2f9ed1b593b7cf6e02c1a9a510faf60fc9dd
VictorCalvo-N <81947996+VictorCalvo-N@users.noreply.github.com>
2021-04-05 00:23:47 -0500

    Create Trabajo1.cpp

---
+++ Trabajo1.cpp
@@ -0,0 +1,65 @@
+//Victor Calvo Guzman
+
+#include <iostream>
+#include <curses.h>
+using namespace std;
+
+int main(){
+int escalon, persona, rpta;
+
+cout << "Ingrese número de escalones: \n";
+  cin >> escalon;
+  cout << "En que escalon estara la persona? \n";
+  cin >> persona;
+    if(persona == escalon){
+      cout << "_P_";
+    }else{
+      cout << "___";
+    }
+     if(escalon == 1){
+        cout << "     ";
+      }
+    for(int i = 0; i < escalon - 1; i++) {
+      cout << "        ";
+      if(i == escalon-2){
+        cout << "     ";
+      }
+    }
+    if(persona == escalon){
+      cout << "_P_\n";
+    }else{
+      cout << "___\n";
+    }
+    for(int j = 0; j < escalon; j++) {
+     for(int k = 0; k < j; k++) {
+       cout << "    ";
+     }
+      if(persona == (escalon-(j+1))){
+        cout << "-" << escalon - j << " |_P_";
+      }
+      else{
+        cout << "-" << escalon - j << " |___";
+      }
+     for(int l = 2; l < escalon - j; l++) {
+        cout << "        ";
+     }
+     if(escalon - j == 1) {
+      cout << "| " << escalon - j << "\n";
+     } else {
+       cout << "    ";
+      if(persona == (escalon-(j+1))){
+        cout << " _p_| " << escalon - j << "\n";
+      }
+      else{
+        cout << " ___| " << escalon - j << "\n";
+      }
+     }
+    }
+cout << "¿Quieres limpiar la pantalla?\n";
+cin >> rpta;
+if (rpta == 1){
+     system("clear");
+}else if (rpta == 2){
+  exit(-1);
+}
+}
